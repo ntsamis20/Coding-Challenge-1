0af1c99be60a13d0073f54f6f2cf6772bec8f976
Nick Tsamis <ntsamis@usf.edu>
2024-09-01 20:39:15 -0400

    Add files via upload

---
+++ Task 2 - Order Management System.js
@@ -0,0 +1,21 @@
+// Task 1: Variables and Data Types
+
+let customerName = "Steve Irwin";
+const customerID = 342;
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
+
+// Task 2: Compound Data Types
+
+let products = ["Windows Computer", "Monitors, Router"];
+const productDetails = {
+    products: "Windows Computer",
+    price: 1799.99,
+    inStock: true
+};
+
+console.log(products);
+console.log(productDetails);
