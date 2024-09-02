e1e462f0277840ee9c75283bfdb69d9a26dc71b1
Nick Tsamis <ntsamis@usf.edu>
2024-09-01 20:54:13 -0400

    Add files via upload

---
+++ Task 4 Employee Performance Evaluation.js
@@ -0,0 +1,46 @@
+// Task 1: Variables and Data Types
+
+let customerName = "Steve Irwin";
+const customerID = 342;
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
+
+// Task 2: Compound Data Types
+
+let products = ["Windows Computer", "Monitors, Router"];
+const productDetails = {
+    products: "Windows Computer",
+    price: 1799.99,
+    inStock: true
+};
+
+console.log(products);
+console.log(productDetails);
+
+// Task 3: Assignment Operators
+
+let accountBalance = 1000;
+accountBalance += 200;
+accountBalance -= 150;
+accountBalance *= 1.05;
+accountBalance /= 1.10;
+accountBalance %= 100;
+
+console.log("Deposited 200:", accountBalance);
+console.log("Withdrew 150:", accountBalance);
+console.log("5% interest rate:", accountBalance);
+console.log("10% Bank fee:", accountBalance);
+console.log("Remainder after dividing by 100:", accountBalance);
+
+// Task 4 Comparison Operators
+
+let employeeScore1 = 65
+let employeescore2 = 91
+console.log("employeeScore1 >= employeeScore2:", employeeScore1 >= employeeScore2);
+console.log("employeeScore1 === employeeScore2:", employeeScore1 === employeeScore2);
+console.log("employeeScore1 > employeeScore2:", employeeScore1 > employeeScore2);
+console.log("employeeScore1 < employeeScore2:", employeeScore1 < employeeScore2);
+console.log("employeeScore1 <= employeeScore2:", employeeScore1 <= employeeScore2);
