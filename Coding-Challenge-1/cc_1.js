9e6797320867268614da10e80be9cb55fb05e769
Nick Tsamis <ntsamis@usf.edu>
2024-09-01 20:26:02 -0400

    Add files via upload

---
+++ Coding-Challenge-1/cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let customerName = "Steve Irwin";
+const customerID = 342;
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
